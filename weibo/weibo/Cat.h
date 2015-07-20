b5783ac0b15ef3b9eb9327831f018c6ce9efea4b
“why” <why@163.com>
2015-07-20 16:09:20 +0800

    创建cat类

---
+++ weibo/weibo/Cat.h
@@ -0,0 +1,13 @@
+//
+//  Cat.h
+//  weibo
+//
+//  Created by xiaomage on 15/7/20.
+//  Copyright (c) 2015年 小码哥. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cat : NSObject
+
+@end
---
+++ weibo/weibo/Cat.m
@@ -0,0 +1,13 @@
+//
+//  Cat.m
+//  weibo
+//
+//  Created by xiaomage on 15/7/20.
+//  Copyright (c) 2015年 小码哥. All rights reserved.
+//
+
+#import "Cat.h"
+
+@implementation Cat
+
+@end
